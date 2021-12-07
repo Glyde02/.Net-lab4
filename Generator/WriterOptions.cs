1c53c700683416aaa4028d455510ec1a66ff6b3a
Glyde <anton123454321045@yandex.by>
2021-12-08 02:38:14 +0300

    GeneratorLib

---
+++ Generator/WriterOptions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneratorLib
+{
+    public class WriterOptions
+    {
+        public string Filename { get; }
+
+        public string Content { get; }
+
+        public WriterOptions(string filename, string content)
+        {
+            Filename = filename;
+            Content = content;
+        }
+    }
+}
