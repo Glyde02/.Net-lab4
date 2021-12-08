9363c35e57d1ba008c514358796fc1b87118fb38
Glyde <anton123454321045@yandex.by>
2021-12-08 03:11:40 +0300

    added tests

---
+++ Test/Generating.cs
@@ -0,0 +1,72 @@
+﻿using GeneratorLib;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Test
+{
+    public class Generating
+    {
+        private const string sourceCode = @"
+        using System;
+
+        namespace SrcFiles
+        {
+            public class Class1
+            {
+                public void Method1()
+                {
+
+                }
+
+                public void Method2()
+                {
+
+                }
+            }
+        }";
+
+        [Fact]
+        public void Count_Num_Of_Test_Classes()
+        {
+            var tests = Generator.GenerateTests(sourceCode);
+
+            Assert.Single(tests);
+        }
+
+        [Fact]
+        public void Correct_Class_Name()
+        {
+            var tests = Generator.GenerateTests(sourceCode).ToArray();
+
+            var className1 = CSharpSyntaxTree.ParseText(tests[0].Content)
+                .GetRoot()
+                .DescendantNodes()
+                .OfType<ClassDeclarationSyntax>()
+                .Single()
+                .Identifier
+                .ValueText;
+
+            Assert.Equal("Class1Test", className1);
+        }
+
+        [Fact]
+        public void Correct_Num_Of_Methods()
+        {
+            var tests = Generator.GenerateTests(sourceCode).ToArray();
+
+            var count = CSharpSyntaxTree.ParseText(tests[0].Content)
+                .GetRoot()
+                .DescendantNodes()
+                .OfType<MethodDeclarationSyntax>()
+                .Count();
+
+            Assert.Equal(2, count);
+        }
+    }
+}
--- lab4/Program.cs
+++ lab4/Program.cs
@@ -8,9 +8,10 @@ namespace lab4
         static void Main(string[] args)
         {
             var generatingOptions = new GeneratingOptions();
-            
-            generatingOptions.SourceDirectory = @"";
-            generatingOptions.DestinationDirectory = @"";
+            Console.WriteLine("Enter source path");
+            generatingOptions.SourceDirectory = @"C:\Users\anton\Documents\study\OOP\lab4\Source\Src";
+            Console.WriteLine("Enter destination path");
+            generatingOptions.DestinationDirectory = @"C:\Users\anton\Documents\study\OOP\lab4\Test";
             Generator.GenerateAsync(generatingOptions).Wait();
             
             
